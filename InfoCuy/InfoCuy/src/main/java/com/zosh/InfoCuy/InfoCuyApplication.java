73eb0130d54c3ad2630e38cadcda4a282339d231
Elvis Arauz <arauzelvis15@gmail.com>
2024-06-19 18:48:23 -0500

    Cambios 1

---
+++ InfoCuy/InfoCuy/src/main/java/com/zosh/InfoCuy/InfoCuyApplication.java
@@ -0,0 +1,13 @@
+package com.zosh.InfoCuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoCuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoCuyApplication.class, args);
+	}
+
+}
---
+++ InfoCuy/InfoCuy/src/test/java/com/zosh/InfoCuy/InfoCuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zosh.InfoCuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoCuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
